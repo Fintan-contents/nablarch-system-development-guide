618409426a66889b8117809c319d32171c33df0f
kumagoro1202 <58413950+kumagoro1202@users.noreply.github.com>
2020-07-14 15:35:19 +0900

    ClimanプロジェクトにAPIのテストを追加 (#17)

---
+++ サンプルプロジェクト/ソースコード/climan-project/src/test/java/com/nablarch/example/climan/rest/common/web/ClimanJacksonBodyConverter.java
@@ -0,0 +1,22 @@
+package com.nablarch.example.climan.rest.common.web;
+
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import nablarch.fw.web.JacksonBodyConverter;
+
+public class ClimanJacksonBodyConverter extends JacksonBodyConverter {
+    public ClimanJacksonBodyConverter() {
+        super(new SnakeCaseObjectMapperFactory());
+    }
+
+    private static class SnakeCaseObjectMapperFactory implements ObjectMapperFactory {
+        @Override
+        public ObjectMapper create() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.getFactory().configure(JsonWriteFeature.ESCAPE_NON_ASCII.mappedFeature(), true);
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+            return objectMapper;
+        }
+    }
+}
