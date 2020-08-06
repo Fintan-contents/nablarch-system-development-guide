91f735023544a6ff9a7d1145519202b70ccdf91d
mseko <masaya.seko@nifty.ne.jp>
2020-08-06 10:32:51 +0900

    checkstyle-exampleを追加翻訳。

---
+++ en/Sample_Project/Sample_Project_Development_Guide/PGUT_Phase/proman-style-guide/java/staticanalysis/checkstyle/checkstyle-example/src/main/java/com/example/MissingJavadocTypeExample.java
@@ -0,0 +1,9 @@
+/*
+ * Header example
+ */
+package com.example;
+
+// Example code for MissingJavadocType
+public class MissingJavadocTypeExample {  // javadoc There are no comments (incorrect).
+}
+
