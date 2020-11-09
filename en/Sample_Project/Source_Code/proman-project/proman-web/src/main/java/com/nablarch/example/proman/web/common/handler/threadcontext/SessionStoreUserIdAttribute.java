426d316d532fc78ba450c525c1aab7e915e99b2f
mseko <masaya.seko@nifty.ne.jp>
2020-11-09 16:41:56 +0900

    Checkstyleのバージョンを8.29へ。合わせてCheckstyleのエラーが出て居た箇所を修正。

--- en/Sample_Project/Source_Code/proman-project/proman-web/src/main/java/com/nablarch/example/proman/web/common/handler/threadcontext/SessionStoreUserIdAttribute.java
+++ en/Sample_Project/Source_Code/proman-project/proman-web/src/main/java/com/nablarch/example/proman/web/common/handler/threadcontext/SessionStoreUserIdAttribute.java
@@ -5,6 +5,11 @@ import nablarch.common.handler.threadcontext.UserIdAttribute;
 import nablarch.common.web.session.SessionUtil;
 import nablarch.fw.ExecutionContext;
 
+/**
+ * User ID attribute to be stored in the thread context.
+ *
+ * @author Masaya Seko
+ */
 public class SessionStoreUserIdAttribute extends UserIdAttribute {
     @Override
     protected Object getUserIdSession(ExecutionContext ctx, String skey) {
