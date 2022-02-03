7ac40a5e058b8eede8f9451c5e6fe76378b2cf42
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-02-03 16:51:03 +0900

    test: JUnit5対応 proman-arch-test

--- サンプルプロジェクト/ソースコード/proman-project/proman-arch-test/src/test/java/com/nablarch/example/proman/arch/ServiceClassRuleTest.java
+++ サンプルプロジェクト/ソースコード/proman-project/proman-arch-test/src/test/java/com/nablarch/example/proman/arch/ServiceClassRuleTest.java
@@ -2,21 +2,18 @@ package com.nablarch.example.proman.arch;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import nablarch.common.dao.DaoContext;
-import org.junit.runner.RunWith;
 
 /**
  * Serviceクラスの実装方法に関するルール。
  */
-@RunWith(ArchUnitRunner.class)
 @AnalyzeClasses(packages = "com.nablarch.example.proman")
-public class ServiceClassRuleTest {
+class ServiceClassRuleTest {
 
     @ArchTest
-    public static final ArchRule DaoContextをfieldに持つ場合private_finalであるがstaticじゃないこと =
+    static final ArchRule DaoContextをfieldに持つ場合private_finalであるがstaticじゃないこと =
             ArchRuleDefinition.fields().that().haveRawType(DaoContext.class)
                 .and().areDeclaredInClassesThat().haveSimpleNameEndingWith("Service")
                 .should().bePrivate()
@@ -27,7 +24,7 @@ public class ServiceClassRuleTest {
      * サービスクラスのDaoContextを引数に持つコンストラクタがpackage privateであること。
      */
     @ArchTest
-    public static final ArchRule DaoContextを引数に持つコンストラクタはpackage_privateであること =
+    static final ArchRule DaoContextを引数に持つコンストラクタはpackage_privateであること =
             ArchRuleDefinition.constructors().that().haveRawParameterTypes(DaoContext.class)
                 .and().areDeclaredInClassesThat().haveSimpleNameEndingWith("Service")
                 .should().bePackagePrivate();
