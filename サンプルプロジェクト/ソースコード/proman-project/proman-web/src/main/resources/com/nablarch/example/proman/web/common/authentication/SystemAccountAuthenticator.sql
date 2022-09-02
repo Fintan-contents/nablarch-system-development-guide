fc6f761701306ae8238d7007bbf06cb686c200eb
ando-mitsuhiro <ando.mitsuhiro.017@tis.co.jp>
2022-09-02 17:30:24 +0900

    予約語を大文字に統一

--- サンプルプロジェクト/ソースコード/proman-project/proman-web/src/main/resources/com/nablarch/example/proman/web/common/authentication/SystemAccountAuthenticator.sql
+++ サンプルプロジェクト/ソースコード/proman-project/proman-web/src/main/resources/com/nablarch/example/proman/web/common/authentication/SystemAccountAuthenticator.sql
@@ -1,7 +1,7 @@
 -- 失敗回数をリセットするSQL
 RESET_FAILED_COUNT =
 UPDATE system_account
-   SET failed_count = case WHEN ? = 0 THEN failed_count ELSE 0 END,
+   SET failed_count = CASE WHEN ? = 0 THEN failed_count ELSE 0 END,
        last_login_date_time = ?
  WHERE user_id = ?
 
