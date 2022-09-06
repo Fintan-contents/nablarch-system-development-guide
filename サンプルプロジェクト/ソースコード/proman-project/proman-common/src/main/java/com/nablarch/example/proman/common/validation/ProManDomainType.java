6f59a7794752d10e8a3237bf2816b6292e340324
ando-mitsuhiro <ando.mitsuhiro.017@tis.co.jp>
2022-09-06 14:41:22 +0900

    ドメイン定義の整理とドメインタイプの最新化

--- サンプルプロジェクト/ソースコード/proman-project/proman-common/src/main/java/com/nablarch/example/proman/common/validation/ProManDomainType.java
+++ サンプルプロジェクト/ソースコード/proman-project/proman-common/src/main/java/com/nablarch/example/proman/common/validation/ProManDomainType.java
@@ -14,86 +14,182 @@ import nablarch.core.validation.ee.SystemChar;
 @SuppressWarnings("all")
 public class ProManDomainType {
 
-    /**
-     * 顧客ID
-     */
-	@Digits(integer = 9)
-    private String clientId;
-
-    /**
-     * 組織ID
-     */
+    /** 組織ID **/
     @Digits(integer = 4)
-    private String organizationId;
+    public String organizationId;
 
-    /**
-     * ログインID
-     */
+    /** ログインID **/
     @Length(max = 20)
     @SystemChar(charsetDef = "半角数字")
-    private String loginId;
+    public String loginId;
 
-    /**
-     * ユーザパスワード
-     */
+    /** ユーザパスワード **/
     @Length(max = 44)
     @SystemChar(charsetDef = "ASCII文字")
-    private String userPassword;
+    public String userPassword;
 
-    /**
-     * プロジェクトID
-     */
+    /** プロジェクトID **/
     @Digits(integer = 9)
-    private String projectId;
+    public String projectId;
 
-    /**
-     * プロジェクト名
-     */
+    /** プロジェクト名 **/
     @Length(max = 128, message = "{domainType.projectName.message}")
     @SystemChar(charsetDef = "全角文字", message = "{domainType.projectName.message}")
-    private String projectName;
+    public String projectName;
 
-    /**
-     * プロジェクト種別
-     */
+    /** プロジェクト種別 **/
     @CodeValue(codeId = "C0300001", pattern = "PATTERN01")
-    private String projectType;
+    public String projectType;
 
-    /**
-     * プロジェクト分類
-     */
+    /** プロジェクト分類 **/
     @CodeValue(codeId = "C0200001", pattern = "PATTERN01")
-    private String projectClass;
+    public String projectClass;
 
-    /**
-     * 日付
-     */
-    @YYYYMMDD()
-    private String date;
+    /** 顧客ID **/
+	@Digits(integer = 9)
+    public String clientId;
 
-    /**
-     * ユーザ氏名（漢字）
-     */
+    /** 日付 **/
+	@YYYYMMDD()
+    public String date;
+
+    /** ユーザ氏名（漢字） **/
     @Length(max = 128, message = "{domainType.userName.message}")
     @SystemChar(charsetDef = "全角文字", message = "{domainType.userName.message}")
-    private String userName;
+    public String userName;
 
-    /**
-     * 備考
-     */
+    /** 備考 **/
     @Length(max = 512, message = "{domainType.note.message}")
     @SystemChar(charsetDef = "システム許容文字", allowLineSeparator = true, message = "{domainType.note.message}")
-    private String note;
+    public String note;
 
-    /**
-     * 金額
-     */
+    /** 金額 **/
     @MoneyRange(max = 999999999)
-    private String amountOfMoney;
+    public String amountOfMoney;
 
-    /**
-     * ページ番号
-     */
+    /** ページ番号 **/
     @NumberRange(min = 1, max = 9999)
-    private String pageNumber;
+    public String pageNumber;
+
+    /** ユーザID **/
+    @NumberRange(max = 999999999)
+    @SystemChar(charsetDef = "半角数字")
+    public String userId;
+
+    /** ユーザID_FK **/
+    @NumberRange(max = 999999999)
+    @SystemChar(charsetDef = "半角数字")
+    public String userIdFk;
+
+    /** 組織ID_FK **/
+    @NumberRange(max = 9999)
+    @SystemChar(charsetDef = "半角数字")
+    public String organizationIdFk;
+
+    /** 組織名 **/
+    @Length(max = 120)
+    @SystemChar(charsetDef = "全角文字")
+    public String organizationName;
+
+    /** プロジェクトID_FK **/
+    @NumberRange(max = 999999999)
+    @SystemChar(charsetDef = "半角数字")
+    public String projectIdFk;
+
+    /** 日時 **/
+    @Length(min = 19, max = 19)
+    @SystemChar(charsetDef = "半角文字")
+    public String dateTime;
+
+    /** 適用開始日 **/
+    @Length(min = 8, max = 8)
+    @SystemChar(charsetDef = "半角文字")
+    public String applyStartDate;
+
+    /** 適用終了日 **/
+    @Length(min = 8, max = 8)
+    @SystemChar(charsetDef = "半角文字")
+    public String applyEndDate;
+
+    /** ユーザ氏名（ふりがな） **/
+    @Length(max = 128)
+    @SystemChar(charsetDef = "全角文字")
+    public String kanaNme;
+
+    /** バージョン番号 **/
+    @SystemChar(charsetDef = "半角数字")
+    public String versionNo;
+
+    /** 失敗回数 **/
+    @SystemChar(charsetDef = "半角数字")
+    public String failedCount;
+
+    /** フラグ **/
+    @Length(min = 1, max = 1)
+    @SystemChar(charsetDef = "半角数字")
+    public String flag;
+
+    /** コードID **/
+    @Length(max = 8)
+    @SystemChar(charsetDef = "半角英数字")
+    public String codeId;
+
+    /** コード値 **/
+    @Length(max = 2)
+    @SystemChar(charsetDef = "半角英数字")
+    public String codeValue;
+
+    /** コード名称 **/
+    @Length(max = 50)
+    @SystemChar(charsetDef = "システム許容文字")
+    public String codeName;
+
+    /** オプション名称 **/
+    @Length(max = 40)
+    @SystemChar(charsetDef = "システム許容文字")
+    public String option;
+
+    /** パターン **/
+    @Length(min = 1, max = 1)
+    @SystemChar(charsetDef = "半角英数字")
+    public String pattern;
+
+    /** ソート順 **/
+    @Length(min = 1, max = 1)
+    @SystemChar(charsetDef = "半角数字")
+    public String sortOrder;
+
+    /** 言語 **/
+    @Length(max = 2)
+    @SystemChar(charsetDef = "半角英字")
+    public String lang;
+
+    /** 略称 **/
+    @Length(max = 50)
+    @SystemChar(charsetDef = "システム許容文字")
+    public String shortName;
+
+    /** セッションID **/
+    @Length(max = 100)
+    @SystemChar(charsetDef = "半角英数字")
+    public String sessionId;
+
+    /** セッションオブジェクト **/
+    public String sessionObject;
+
+    /** トークン **/
+    @Length(max = 36)
+    @SystemChar(charsetDef = "半角英数字")
+    public String token;
+
+    /** 区分 **/
+    @Length(min = 2, max = 2)
+    @SystemChar(charsetDef = "半角英数字")
+    public String segmentId;
+
+    /** 業務日付 **/
+    @Length(min = 8, max = 8)
+    @SystemChar(charsetDef = "半角数字")
+    public String bizDate;
+
 }
