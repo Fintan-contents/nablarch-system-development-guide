ea0193c0015f52f19c1c1b026d7ac2a6ac412dc8
ando-mitsuhiro <ando.mitsuhiro.017@tis.co.jp>
2022-09-21 17:40:39 +0900

    項目名のキャメルケース対応

--- サンプルプロジェクト/ソースコード/climan-project/src/main/java/com/nablarch/example/climan/common/jaxrs/ClimanJackson2BodyConverter.java
+++ サンプルプロジェクト/ソースコード/climan-project/src/main/java/com/nablarch/example/climan/common/jaxrs/ClimanJackson2BodyConverter.java
@@ -15,7 +15,6 @@ public class ClimanJackson2BodyConverter  extends Jackson2BodyConverter {
     @Override
     protected void configure(ObjectMapper objectMapper) {
         super.configure(objectMapper);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
     }
 
     /**
