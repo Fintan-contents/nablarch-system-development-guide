f299d92108c169c31a5d624f2d44dd68f0051089
ando-mitsuhiro <110872739+ando-mitsuhiro@users.noreply.github.com>
2022-09-30 13:42:48 +0900

    Nab 458 samples client management en (#113)
    * Reflection of corrections in the Japanese version
    
    * Revision history Update
    
    * New Creation
    
    * New Creation
    
    * Reflection of corrections in the Japanese version
    
    * Corrected component ID
    
    * Revised document naming rules to be consistent
    
    * 一貫性を保つために文書の命名規則に則りファイル名を修正
    
    * 目的・概要の修正漏れ対応

--- en/Sample_Project/Source_Code/climan-project/src/main/java/com/nablarch/example/climan/common/validation/ClimanDomainType.java
+++ en/Sample_Project/Source_Code/climan-project/src/main/java/com/nablarch/example/climan/common/validation/ClimanDomainType.java
@@ -22,4 +22,44 @@ public class ClimanDomainType {
     /** Industry code */
     @CodeValue(codeId = "C0100001")
     private String industryCode;
+
+    /** Code ID **/
+    @Length(max = 8)
+    @SystemChar(charsetDef = "半角英数字") // Alphanumeric characters
+    public String codeId;
+
+    /** Code Value **/
+    @Length(max = 2)
+    @SystemChar(charsetDef = "半角英数字") // Alphanumeric characters
+    public String codeValue;
+
+    /** Code Name **/
+    @Length(max = 50)
+    @SystemChar(charsetDef = "システム許容文字") // Characters permitted by the system
+    public String codeName;
+
+    /** Option **/
+    @Length(max = 40)
+    @SystemChar(charsetDef = "システム許容文字") // Characters permitted by the system
+    public String option;
+
+    /** Pattern **/
+    @Length(min = 1, max = 1)
+    @SystemChar(charsetDef = "半角英数字") // Alphanumeric characters
+    public String pattern;
+
+    /** Sort Order **/
+    @Length(min = 1, max = 1)
+    @SystemChar(charsetDef = "半角数字") // Numbers
+    public String sortOrder;
+
+    /** Lang **/
+    @Length(max = 2)
+    @SystemChar(charsetDef = "半角英字") // Half-width alphabetic characters    
+    public String lang;
+
+    /** Shor tName **/
+    @Length(max = 50)
+    @SystemChar(charsetDef = "システム許容文字") // Characters permitted by the system
+    public String shortName;
 }
