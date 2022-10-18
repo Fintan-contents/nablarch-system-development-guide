0266fa7246fe5e127f5b1650a0a8cee844c078a4
ando-mitsuhiro <ando.mitsuhiro.017@tis.co.jp>
2022-10-18 19:25:22 +0900

    テストサポートを廃止 テスト対象メソッドをpublicに変更したため呼び出し方法を修正 アサーションクラスを変更 ケースデータを変更

--- サンプルプロジェクト/ソースコード/proman-project/proman-web/src/test/java/com/nablarch/example/proman/web/project/ProjectUpdateFormTest.java
+++ サンプルプロジェクト/ソースコード/proman-project/proman-web/src/test/java/com/nablarch/example/proman/web/project/ProjectUpdateFormTest.java
@@ -1,16 +1,14 @@
 package com.nablarch.example.proman.web.project;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
-import com.nablarch.example.proman.web.test.FormTestSupport;
 
 /**
  * ProjectUpdateFormのテスト
  *
  */
-class ProjectUpdateFormTest extends FormTestSupport<ProjectUpdateForm> {
+class ProjectUpdateFormTest {
 
     /**
      * 正常系（プロジェクト開始日＜プロジェクト終了日）
@@ -20,8 +18,8 @@ class ProjectUpdateFormTest extends FormTestSupport<ProjectUpdateForm> {
         ProjectUpdateForm form = new ProjectUpdateForm();
         form.setProjectStartDate("2022/01/01");
         form.setProjectEndDate("2022/01/31");
-        boolean result = (Boolean) doMethod(ProjectUpdateForm.class, "isValidProjectPeriod", form);
-        assertTrue("プロジェクト開始日＜プロジェクト終了日", result);
+        boolean result = form.isValidProjectPeriod();
+        assertTrue(result, "プロジェクト開始日＜プロジェクト終了日");
     }
 
     /**
@@ -32,8 +30,8 @@ class ProjectUpdateFormTest extends FormTestSupport<ProjectUpdateForm> {
         ProjectUpdateForm form = new ProjectUpdateForm();
         form.setProjectStartDate("2022/01/01");
         form.setProjectEndDate("2022/01/01");
-        boolean result = (Boolean) doMethod(ProjectUpdateForm.class, "isValidProjectPeriod", form);
-        assertTrue("プロジェクト開始日＝プロジェクト終了日", result);
+        boolean result = form.isValidProjectPeriod();
+        assertTrue(result, "プロジェクト開始日＝プロジェクト終了日");
     }
 
     /**
@@ -44,7 +42,7 @@ class ProjectUpdateFormTest extends FormTestSupport<ProjectUpdateForm> {
         ProjectUpdateForm form = new ProjectUpdateForm();
         form.setProjectStartDate("2022/01/31");
         form.setProjectEndDate("2022/01/01");
-        boolean result = (Boolean) doMethod(ProjectUpdateForm.class, "isValidProjectPeriod", form);
-        assertFalse("プロジェクト開始日＞プロジェクト終了日", result);
+        boolean result = form.isValidProjectPeriod();
+        assertFalse(result, "プロジェクト開始日＞プロジェクト終了日");
     }
 }
