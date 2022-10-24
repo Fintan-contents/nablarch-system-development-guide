df9958d6115b4fa4a8d9951b2c14091fe64ed59f
ando-mitsuhiro <ando.mitsuhiro.017@tis.co.jp>
2022-10-24 12:37:21 +0900

    テスト仕様書に合わせてテストケースを追加

--- サンプルプロジェクト/ソースコード/climan-project/src/test/java/com/nablarch/example/climan/rest/client/ClientServiceTest.java
+++ サンプルプロジェクト/ソースコード/climan-project/src/test/java/com/nablarch/example/climan/rest/client/ClientServiceTest.java
@@ -6,6 +6,7 @@ import com.nablarch.example.climan.entity.Client;
 import com.nablarch.example.climan.rest.common.dao.DaoStub;
 import com.nablarch.example.climan.rest.common.repository.ConfigLoader;
 import nablarch.common.dao.EntityList;
+import nablarch.common.dao.NoDataException;
 import nablarch.core.repository.SystemRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -25,30 +26,6 @@ class ClientServiceTest {
     @BeforeAll
     static void setUp() {
         ConfigLoader.load();
-    }
-
-    /**
-     * 顧客検索で検索結果が上限件数を超える場合のテスト。
-     *
-     * {@link SearchResultUpperLimitException}が送出され、上限件数が設定されていること。
-     */
-    @Test
-    void testFindClientForSearchResultUpperLimitError() {
-
-        ClientService sut = new ClientService(new DaoStub() {
-            @Override
-            public <T> long countBySqlFile(Class<T> entityClass, String sqlId, Object params) {
-                return Long.valueOf(SystemRepository.get("app.common.search.limit")) + 1;
-            }
-        });
-
-        try {
-            sut.findClient(new Client());
-            fail();
-        } catch (SearchResultUpperLimitException e) {
-            long expected = Long.valueOf(SystemRepository.get("app.common.search.limit"));
-            assertEquals(expected, e.getLimit());
-        }
     }
 
     /**
@@ -76,20 +53,82 @@ class ClientServiceTest {
     }
 
     /**
-     * 顧客登録で既に登録されているデータと重複する場合のテスト。
+     * 顧客検索で検索結果が上限件数を超える場合のテスト。
      *
-     * {@link DuplicateRegistrationException}が送出される。
+     * {@link SearchResultUpperLimitException}が送出され、上限件数が設定されていること。
      */
     @Test
-    void testRegisterClientForDuplicateRegistrationException() {
+    void testFindClientForSearchResultUpperLimitError() {
+
         ClientService sut = new ClientService(new DaoStub() {
             @Override
             public <T> long countBySqlFile(Class<T> entityClass, String sqlId, Object params) {
-                return 1;
+                return Long.valueOf(SystemRepository.get("app.common.search.limit")) + 1;
             }
         });
 
-        Assertions.assertThrows(DuplicateRegistrationException.class, () -> sut.registerClient(new Client()));
+        try {
+            sut.findClient(new Client());
+            fail();
+        } catch (SearchResultUpperLimitException e) {
+            long expected = Long.valueOf(SystemRepository.get("app.common.search.limit"));
+            assertEquals(expected, e.getLimit());
+        }
+    }
+
+    /**
+     * 顧客詳細取得で検索結果が１件存在する場合のテスト。
+     *
+     * 検索結果が返されること。
+     */
+    @Test
+    void testFindClientById() {
+        final Map<String, Boolean> invoked = new HashMap<String, Boolean>();
+        Client client = new Client();
+        client.setClientId(1);
+        client.setClientName("顧客１");
+        client.setIndustryCode("01");
+        ClientService sut = new ClientService(new DaoStub() {
+            @SuppressWarnings({ "unchecked" })
+            @Override
+            public <T> T findById(Class<T> entityClass, Object... id) {
+                invoked.put("findById", true);
+                return (T)  client;
+            }
+
+            @Override
+            public <T> long countBySqlFile(Class<T> entityClass, String sqlId, Object params) {
+                return Long.valueOf(SystemRepository.get("app.common.search.limit"));
+            }
+        });
+
+        Client result = sut.findClientById(1);
+        assertTrue(invoked.get("findById"));
+        assertEquals(client, result);
+    }
+
+    /**
+     * 顧客詳細取得で検索結果存在しない場合のテスト。
+     *
+     * {@link NoDataException}が送出されること。
+     */
+    @Test
+    void testFindClientByIdForNoDataException() {
+        final Map<String, Boolean> invoked = new HashMap<String, Boolean>();
+        ClientService sut = new ClientService(new DaoStub() {
+            @Override
+            public <T> T findById(Class<T> entityClass, Object... id) {
+                invoked.put("findById", true);
+                throw new NoDataException();
+            }
+        });
+
+        try {
+            sut.findClientById(0);
+            fail();
+        } catch (NoDataException e) {
+            assertTrue(invoked.get("findById"));
+        }
     }
 
     /**
@@ -127,5 +166,22 @@ class ClientServiceTest {
 
         assertTrue(invoked.containsKey("insert"));
         assertEquals(client, result);
+    }
+
+    /**
+     * 顧客登録で既に登録されているデータと重複する場合のテスト。
+     *
+     * {@link DuplicateRegistrationException}が送出される。
+     */
+    @Test
+    void testRegisterClientForDuplicateRegistrationException() {
+        ClientService sut = new ClientService(new DaoStub() {
+            @Override
+            public <T> long countBySqlFile(Class<T> entityClass, String sqlId, Object params) {
+                return 1;
+            }
+        });
+
+        Assertions.assertThrows(DuplicateRegistrationException.class, () -> sut.registerClient(new Client()));
     }
 }
