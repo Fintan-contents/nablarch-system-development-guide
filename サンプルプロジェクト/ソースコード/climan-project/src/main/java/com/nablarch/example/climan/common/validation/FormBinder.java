08fd40fe437e5c5b740fd37922dd0789c63facdf
kankichi <ichikawa.kankichi@tis.co.jp>
2024-06-21 16:26:47 +0900

    5u23で追加されたBean Validationのグループ機能が原因で、コンパイルエラーが発生するため解消

--- サンプルプロジェクト/ソースコード/climan-project/src/main/java/com/nablarch/example/climan/common/validation/FormBinder.java
+++ サンプルプロジェクト/ソースコード/climan-project/src/main/java/com/nablarch/example/climan/common/validation/FormBinder.java
@@ -174,7 +174,11 @@ public class FormBinder<T extends Serializable> {
             public String validate() {
                 return "";  // 使用しない。
             }
+
+            @Override
+            public Class<?>[] validationGroup() {
+                return new Class<?>[0];  // 使用しない。 }
+            }
         };
     }
-
 }
